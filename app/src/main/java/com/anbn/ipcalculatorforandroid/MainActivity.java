284cc41b55e8fb1932506f6794523a91eae0edb6
GTemp <anbngm@gmail.com>
2022-01-28 12:43:11 +0500

    First commit on test branch

---
+++ app/src/main/java/com/anbn/ipcalculatorforandroid/MainActivity.java
@@ -0,0 +1,59 @@
+package com.anbn.ipcalculatorforandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.anbn.ipcalculatorforandroid.R;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem tabItem1, tabItem2, tabItem3, tabItem4;
+    ViewPager viewPager;
+    com.anbn.ipcalculatorforandroid.PageAdapter pageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout1);
+        tabItem1 = (TabItem) findViewById(R.id.tab1);
+        tabItem2 = (TabItem) findViewById(R.id.tab2);
+        tabItem3 = (TabItem) findViewById(R.id.tab3);
+        tabItem4 = (TabItem) findViewById(R.id.tab4);
+        viewPager = (ViewPager) findViewById(R.id.vpager);
+
+        pageAdapter = new com.anbn.ipcalculatorforandroid.PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                if(tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 2 || tab.getPosition() == 3)
+                    pageAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        // listen for scroll or page change
+
+    }
+}
