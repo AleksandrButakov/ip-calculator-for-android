0e49dddb537296a5b51a2297224b279d6d3431b4
GTemp <anbngm@gmail.com>
2022-01-28 20:31:37 +0500

    Layout of fragment_1

--- app/src/main/java/com/anbn/ipcalculatorforandroid/MainActivity.java
+++ app/src/main/java/com/anbn/ipcalculatorforandroid/MainActivity.java
@@ -4,29 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.anbn.ipcalculatorforandroid.R;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
+import org.w3c.dom.Text;
+
+import java.text.BreakIterator;
+
 public class MainActivity extends AppCompatActivity {
 
-    TabLayout tabLayout;
-    TabItem tabItem1, tabItem2, tabItem3, tabItem4;
-    ViewPager viewPager;
+    //TabLayout tabLayout;
+    //TabItem tabItem1, tabItem2, tabItem3, tabItem4;
+    //ViewPager viewPager;
     com.anbn.ipcalculatorforandroid.PageAdapter pageAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabLayout = (TabLayout) findViewById(R.id.tablayout1);
-        tabItem1 = (TabItem) findViewById(R.id.tab1);
-        tabItem2 = (TabItem) findViewById(R.id.tab2);
-        tabItem3 = (TabItem) findViewById(R.id.tab3);
-        tabItem4 = (TabItem) findViewById(R.id.tab4);
-        viewPager = (ViewPager) findViewById(R.id.vpager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout1);
+        TabItem tabItem1 = (TabItem) findViewById(R.id.tab1);
+        TabItem tabItem2 = (TabItem) findViewById(R.id.tab2);
+        TabItem tabItem3 = (TabItem) findViewById(R.id.tab3);
+        TabItem tabItem4 = (TabItem) findViewById(R.id.tab4);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vpager);
+
 
         pageAdapter = new com.anbn.ipcalculatorforandroid.PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(pageAdapter);
@@ -35,25 +45,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-
                 if(tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 2 || tab.getPosition() == 3)
                     pageAdapter.notifyDataSetChanged();
-
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
             }
         });
 
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         // listen for scroll or page change
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
     }
+
+    public void button2 (View v) {
+        Toast.makeText(getBaseContext(), "Выберите оборудование, " +
+                        "интефейсную плату и нажмите кнопку ...",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void ipAddress1 (View v) {
+        EditText ipAddress = findViewById(R.id.ipAddress1);
+        ipAddress.setText("");
+
+
+    }
+
+
 }
